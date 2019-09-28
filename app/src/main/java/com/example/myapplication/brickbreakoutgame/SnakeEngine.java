459b1855ff452249cf9e5cd4df74718104073d44
emilylakic <emilylakic@gmail.com>
2019-09-27 23:54:18 -0400

    Extend SurfaceView, add method for run

--- app/src/main/java/com/example/myapplication/brickbreakoutgame/SnakeEngine.java
+++ app/src/main/java/com/example/myapplication/brickbreakoutgame/SnakeEngine.java
@@ -1,4 +1,31 @@
 package com.example.myapplication.brickbreakoutgame;
 
-public class SnakeEngine {
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Point;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import java.io.IOException;
+import java.util.Random;
+import android.content.res.AssetManager;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+private Thread thread = null;
+private Context context;
+
+public class SnakeEngine extends SurfaceView implements Runnable {
+
+    public SnakeEngine(Context context, Point size) {
+        super(context);
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
