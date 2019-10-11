cd0de297af5f5acdcfa6e7991a4277733a073049
emilylakic <emilylakic@gmail.com>
2019-10-11 10:42:54 -0400

    Reset MainActivity.Java

--- app/src/main/java/com/example/myapplication/brickbreakoutgame/MainActivity.java
+++ app/src/main/java/com/example/myapplication/brickbreakoutgame/MainActivity.java
@@ -1,32 +1,12 @@
 package com.example.myapplication.brickbreakoutgame;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.graphics.Point;
 import android.os.Bundle;
-import android.view.Display;
 
 public class MainActivity extends AppCompatActivity {
-    SnakeEngine snakeEngine;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        snakeEngine = new SnakeEngine(this, size);
-        setContentView(snakeEngine);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        snakeEngine.resume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        snakeEngine.pause();
+        setContentView(R.layout.activity_main);
     }
 }
